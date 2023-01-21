f61531494fe2eacce488a52b8f479fe4c7c952c9
Kaushik-jagana <jaganakaushik@gmail.com>
2023-01-21 12:32:12 +0530

    Connecting API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from "react";
+import { useEffect} from "react";
+
+const API_URL = 'http://www.omdbapi.com?apikey=cfb42f45';
 
 const App =() =>{
+   
+    const searchmovies =async(title)=>{
+        const resonse= await fetch(`${API_URL}&s=${title}`);
+        const data = await resonse.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(()=>{
+      searchmovies('spiderman');
+    },[]);
+
     return(
         <h1> App </h1>
     );
